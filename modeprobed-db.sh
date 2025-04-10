23cd989a44a97729b3687464e404ceb4a2193666
Jackblow33 <jackblow33@hotmail.com>
2025-04-09 23:18:21 -0400

    Update modeprobed-db.sh

--- modeprobed-db.sh
+++ modeprobed-db.sh
@@ -4,4 +4,14 @@ git clone https://github.com/graysky2/modprobed-db.git
 cd /modeprobed-db
 sudo make & sudo make install
 modprobed-db store
+
+#create cron task
+sudo apt install cronie
+
+
+
+
+
 modprobed-db list
+
+
