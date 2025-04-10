8be2d600949535b29eb922585e528a506f3b03a9
Jackblow33 <jackblow33@hotmail.com>
2025-04-09 23:33:53 -0400

    Update modeprobed-db.sh

--- modeprobed-db.sh
+++ modeprobed-db.sh
@@ -8,9 +8,12 @@ modprobed-db store
 # Create cron task every hour execute modprobed-db
 sudo apt install cronie
 crontab -e
-# Add this line
-0 */1 * * *   /usr/bin/modprobed-db store &> /dev/null
-
+clear
+echo
+echo
+echo 'Add this line'
+echo '0 */1 * * *   /usr/bin/modprobed-db store &> /dev/null'     #0 */1 * * *   /usr/bin/modprobed-db store &> /dev/null
+echo "Press [enter] key to continue. . ."; read enterKey
 # Heres the first list of kernel modules.
 modprobed-db list
 
