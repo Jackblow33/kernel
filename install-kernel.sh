878b355b132e1c932526a1d31ca5de3f508558d6
Jackblow33 <jackblow33@hotmail.com>
2025-04-10 21:19:36 -0400

    Update install-kernel.sh

--- install-kernel.sh
+++ install-kernel.sh
@@ -2,8 +2,13 @@
 
 KERNEL=6.14.1-tkg-eevdf
 
-cd /home/jack/linux-tkg/DEBS
+#cd /home/jack/linux-tkg/DEBS
+sudo mkdir /mnt/usb
+#lsblk
+sudo mount /dev/sdd1 /mnt/usb   #choose appropriate path
+cd /mnt/usb/_MyFiles/kernels/'6.14-1 tkg'
 sudo dpkg -i *.deb
 
-sudo update-initramfs -c -k $KERNEL
-sudo update-grub
+# In case your system is not booting to the new kernel, you can update initramfs and your GRUB configuration manually
+#sudo update-initramfs -c -k $KERNEL
+#sudo update-grub
